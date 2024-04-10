a57001933acb8e1c342283f42f606c99eaea79be
JeremMac <macal.jerem@gmail.com>
2024-04-10 19:43:12 +0200

    Added a program that print combinations of numbers.

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 10)
+                {
+                    j++;
+                }
+                Console.Write($"{j}{i}, ");
+                if (j + i == 17)
+                {
+                    Console.Write("99\n");
+                }
+            }
+        }
+    }
+}
