b49d95d8f1a2d26dd677bc52095e7b0ad066076d
JeremMac <macal.jerem@gmail.com>
2024-04-11 10:54:18 +0200

    Added a program that return the last digit of a number.

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+        {
+            number = number * -1;
+        }
+        int last_digit = number % 10;
+        Console.Write(last_digit);
+        return last_digit;
+    }
+}
