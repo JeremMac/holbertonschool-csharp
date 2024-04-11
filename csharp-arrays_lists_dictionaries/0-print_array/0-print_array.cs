fc31e7c98f6a51c35f36bc034b58ca51a380acc4
JeremMac <macal.jerem@gmail.com>
2024-04-11 14:24:27 +0200

    Changed Write for WriteLine at line 14.

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -11,7 +11,7 @@ class Array
         }
         if (size == 0)
         {
-            Console.Write("");
+            Console.WriteLine("");
         }
         int [] array = new int[size];
         for (int i = 0; i < size; i++)
