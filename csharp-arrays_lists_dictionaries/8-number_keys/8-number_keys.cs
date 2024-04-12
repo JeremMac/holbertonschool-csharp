da8ddcdca51810600920a797a7ca4ca8a098a51a
JeremMac <macal.jerem@gmail.com>
2024-04-12 12:11:13 +0200

    Added a program that cont the number of keys in a dictionary.

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,23 @@
+﻿using System
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int keys = 0;
+        if (myDict != null)
+        {
+            foreach(var key in myDict)
+            {
+                keys++;
+            }
+            return keys;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
