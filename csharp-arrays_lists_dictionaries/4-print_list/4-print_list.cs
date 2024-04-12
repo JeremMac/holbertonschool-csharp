c08fa2e05e742fd6121c8b4bb61da804431b2795
JeremMac <macal.jerem@gmail.com>
2024-04-12 09:53:27 +0200

    Added  a program that return and print a new filled list.

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> list = new List<int>();
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+            if (i != size - 1)
+            {
+                Console.Write(list[i] + " ");
+            }
+            else if (i == size - 1)
+            {
+                Console.WriteLine(list[i]);
+            }
+            else
+            {
+                Console.Write(list[i]);
+            }
+        }
+        return list;
+    }
+}
