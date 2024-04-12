ba7406f54d6cc9d4d2f83b037f3ab639c834c10a
JeremMac <macal.jerem@gmail.com>
2024-04-12 15:18:46 +0200

    Added a program that return the key with the highest value in a dictionary.

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null)
+        {
+            return "None";
+        }
+        int max = 0;
+        string maxKey = "";
+        foreach(KeyValuePair<string, int> item in myList)
+        {
+            if (item.Value > max)
+            {
+                max = item.Value;
+                maxKey = item.Key;
+            }
+        }
+        return maxKey;
+    }
+}
