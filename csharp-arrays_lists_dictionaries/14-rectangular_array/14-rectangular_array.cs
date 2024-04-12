995f4747f220f96cd8592d88c8ab492548cae145
JeremMac <macal.jerem@gmail.com>
2024-04-12 15:59:29 +0200

    Changed code.

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 
-class Program
-{
-    static void Main()
-    {
-        // Create a 5 by 5 two-dimensional array
-        int[,] array = new int[5, 5];
+class Program {
+    public static void Main() {
+        int [,] rectangularArray = new int[5, 5];
 
-        // Initialize index [2,2] to 1 and all other indices to 0
-        array[2, 2] = 1;
+        rectangularArray[2,2] = 1;
 
-        // Print the array
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                Console.Write(array[i, j] + " ");
+        for (int i = 0; i < rectangularArray.GetLength(0); i++) {
+            for (int j = 0; j < rectangularArray.GetLength(1); j++) {
+                if (j < rectangularArray.GetLength(1) - 1) {
+                    Console.Write(rectangularArray[i, j] + " ");
+                }
+                else {
+                    Console.Write(rectangularArray[i, j]);
+                }
             }
             Console.WriteLine();
         }
