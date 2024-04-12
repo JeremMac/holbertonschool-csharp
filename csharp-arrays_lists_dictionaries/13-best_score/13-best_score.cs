5cbf7e544af6ef2f6f2c08d322238a80aa0c452e
JeremMac <macal.jerem@gmail.com>
2024-04-12 15:27:30 +0200

    Changed none condition.

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -6,7 +6,7 @@ class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
-        if (myList == null)
+        if (myList == null || myList.Count == 0)
         {
             return "None";
         }
