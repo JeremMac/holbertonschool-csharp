c42cc5a1e99581ed76311cd93309f530a0584a30
JeremMac <macal.jerem@gmail.com>
2024-04-12 15:33:11 +0200

    Changed if condition.

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -6,20 +6,20 @@ class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
-        if (myList == null || myList.Count == 0)
+        if (myList != null && myList.Count != 0)
         {
-            return "None";
-        }
         int max = 0;
         string maxKey = "";
-        foreach(KeyValuePair<string, int> item in myList)
-        {
-            if (item.Value > max)
+            foreach(KeyValuePair<string, int> item in myList)
             {
-                max = item.Value;
-                maxKey = item.Key;
+                if (item.Value > max)
+                {
+                    max = item.Value;
+                    maxKey = item.Key;
+                }
             }
+            return maxKey;
         }
-        return maxKey;
+        return "None";
     }
 }
