579a8cec24fd22f8a8c2f38db11159d3602d4c65
JeremMac <macal.jerem@gmail.com>
2024-04-15 14:50:55 +0200

    Added a method that returns a sorted list of all elements present in one or the other list.

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> newlist = new List<int>();
+        foreach  (int i in list1)
+        {
+            if (list2.Contains(i) == false)
+            {
+                newlist.Add(i);
+            }
+        }
+        foreach (int i in list2)
+        {
+            if (!list1.Contains(i))
+            {
+                newlist.Add(i);
+            }
+        }
+        newlist.Sort();
+        return newlist;
+    }
+}
