168e5a9058b3f4c96d13e0ea510d653cd0df8f48
JeremMac <macal.jerem@gmail.com>
2024-04-15 13:27:14 +0200

    Added a program that  that adds all unique integers in a list.

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> list = new List<int>();
+
+        foreach (int i in list1)
+        {
+            if (list2.Contains(i))
+            {
+                list.Add(i);
+            }
+        }
+        list.Sort();
+        return list;
+    }
+}
