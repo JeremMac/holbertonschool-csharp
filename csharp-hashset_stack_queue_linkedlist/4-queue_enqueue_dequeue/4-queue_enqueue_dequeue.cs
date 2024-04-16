53d276483c77452038cb2ed673841ea7eb5bdabd
JeremMac <macal.jerem@gmail.com>
2024-04-16 10:53:27 +0200

    Added a program that execute multiple tasks on a queue.

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        if (aQueue.Count == 0 || aQueue == null)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        }
+        aQueue.Enqueue(newItem);
+        if (aQueue.Contains(search))
+        {
+            Console.WriteLine($"Queue contains {search}: True");
+            while(aQueue.Dequeue() != search)
+             {
+                
+             }
+        }
+        else if (!aQueue.Contains(search))
+        {
+            Console.WriteLine($"Queue contains {search}: False");
+        }
+        return aQueue;
+    }
+}
