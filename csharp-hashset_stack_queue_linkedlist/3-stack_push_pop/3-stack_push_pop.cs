615e97ecc90000da918cd10f789f6fe1629c08d8
JeremMac <macal.jerem@gmail.com>
2024-04-16 09:50:53 +0200

    Place push command into a condition.

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -17,6 +17,7 @@ class MyStack
             while (aStack.Pop() != search)
             {
             }
+            aStack.Push(newItem);
         }
         else if (!aStack.Contains(search))
         {
