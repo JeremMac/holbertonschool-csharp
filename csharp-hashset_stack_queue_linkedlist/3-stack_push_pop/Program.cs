be709b769758bf9cfd59a2ee107261edb80a0727
JeremMac <macal.jerem@gmail.com>
2024-04-16 09:42:54 +0200

    Added a program that perform multiple operations on a stack.

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        Console.WriteLine($"Top item: {aStack.Peek()}");
+        if (aStack.Count <= 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        if (aStack.Contains(search))
+        {
+            Console.WriteLine($"Stack contains \"{search}\": True");
+            while (aStack.Pop() != search)
+            {
+            }
+        }
+        else if (!aStack.Contains(search))
+        {
+            Console.WriteLine($"Stack contains \"{search}\": False");
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
