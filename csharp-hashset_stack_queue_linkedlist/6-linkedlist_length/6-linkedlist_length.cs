27840abfa7a8404bebaef820fc7f2a277a7ad0e8
JeremMac <macal.jerem@gmail.com>
2024-04-16 11:53:42 +0200

    Added  a method that returns the number of elements in a LinkedList.

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,12 @@
+﻿class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        foreach (var item in myLList)
+        {
+            count++;
+        }
+        return count;
+    }
+}
