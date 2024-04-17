c66f52be2c6a579de123b5c1922ad4dc9e6656a1
JeremMac <macal.jerem@gmail.com>
2024-04-17 09:42:59 +0200

    Added a method that returns the sum of all the data of a LinkedList.

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        if (myLList.Count > 0 || myLList != null)
+        {
+            foreach (int item in myLList)
+            {
+                sum += item;
+            }
+        }
+        return sum;
+    }
+}
