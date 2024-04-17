0a2b47bded93b4e9efd565e00215c29846373e69
JeremMac <macal.jerem@gmail.com>
2024-04-17 13:46:31 +0200

    deleted null case condition.

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,14 +5,11 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
-        if (myLList.Count > 0 || index > 0)
-        {
             LinkedListNode<int> currentNode = myLList.First;
             for (int i = 0; i < index; i++)
             {
                 currentNode = currentNode.Next;
             }
             myLList.Remove(currentNode);
-        }
     }
 }
