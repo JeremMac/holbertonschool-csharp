29cd93575fe6d9c8cbc6e28c27896116cbf5186d
JeremMac <macal.jerem@gmail.com>
2024-05-08 18:55:43 +0200

    Added an overriden ToString method to my structure.

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public Dog(string Name, float Age, string Owner, Rating Rating)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rating;
+    }
+
+    public string name {get; }
+    public float age {get; }
+    public string owner {get; }
+    public Rating rating {get; }
+
+    public override string ToString()
+    {
+        return $"Dog Name: {name}\n" + $"Age: {age}\n" + $"Owner: {owner}\n" + $"Rating: {rating}";
+    }
+}
