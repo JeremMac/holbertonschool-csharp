be169a33469122f7756dbbd89db891ddd8beb613
JeremMac <macal.jerem@gmail.com>
2024-05-08 13:54:02 +0200

    Added a program that print a list using try and catch.

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        try
+        {
+            while(count < n)
+            {
+                Console.WriteLine(myList[count]);
+                count++;
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+        return count;
+    }
+}
