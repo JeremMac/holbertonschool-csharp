fa2b9e1fa6623a3d21f01fb26c21f2a6f9c07122
JeremMac <macal.jerem@gmail.com>
2024-05-09 18:51:39 +0200

    Added a constructor to the zombie class.

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///Sets a Zombie class with no health.
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        ///public health field.
+        ///</summary>
+        public int health;
+
+        ///<summary>
+        ///Constructor to set health value.
+        ///</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        
+        ///<summary>
+        ///Constructor to verifie that value is not inferior to zero.
+        ///</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
