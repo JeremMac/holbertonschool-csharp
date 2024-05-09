dfd2b5b5312d8af0b775aa147f2deb455f12b76d
JeremMac <macal.jerem@gmail.com>
2024-05-09 19:14:48 +0200

    Added a method thet returns the value of health.

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///Sets a Zombie class with no health.
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        ///public health field.
+        ///</summary>
+        private int health;
+
+        ///<summary>
+        ///Constructor to set health value.
+        ///</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        
+        ///<summary>
+        ///Constructor to verifie that value is not inferior to zero.
+        ///</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        
+        ///<summary>
+        ///a method that returnst the value of health.
+        ///</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
