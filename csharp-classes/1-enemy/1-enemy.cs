688243055e0888cd0b58ef764fe8d70f11b2310a
JeremMac <macal.jerem@gmail.com>
2024-05-09 18:27:02 +0200

    Added an health field to enemy zombie class.

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///Sets a Zombie class with no health.
+    ///</summary>
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie(int Health = 0)
+        {
+            health = Health;
+        }
+    }
+}
