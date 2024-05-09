95dd114002a78326889ebd063343447bab8ef995
JeremMac <macal.jerem@gmail.com>
2024-05-09 18:35:57 +0200

    Added commentaries.

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -7,8 +7,14 @@ namespace Enemies
     ///</summary>
     public class Zombie
     {
+        ///<summary>
+        ///public health field.
+        ///</summary>
         public int health;
 
+        ///<summary>
+        ///Constructor to set health value.
+        ///</summary>
         public Zombie()
         {
             health = 0;
