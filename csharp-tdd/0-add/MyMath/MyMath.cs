c8b0575ca4091c64bbb142c96c514f7c8faec6df
JeremMac <macal.jerem@gmail.com>
2024-05-10 16:24:32 +0200

    Added forgotten documentation.

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,7 +1,13 @@
 ﻿namespace MyMath;
 
+///<summary>
+///A class that contains the Add method.
+///</summary>
 public class Operations
 {
+    ///<summary>
+    ///A method that adds two integers.
+    ///</summary>
     public static int Add(int a, int b)
     {
         return a + b;
