0b041ddac49d6319bbb4ce4235306e331c1a89dd
JeremMac <macal.jerem@gmail.com>
2024-05-14 14:25:07 +0200

    Added a class that trows an exception.

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+///<summary>
+/// A class that contains the Area method.
+///</summary>
+class Shape
+{
+
+    ///<summary>
+    /// A method that throws an error message.
+    ///</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
