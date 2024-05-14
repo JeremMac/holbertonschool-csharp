3dc255ae4e85775fc2e9a51208db3a2feb7f23a8
JeremMac <macal.jerem@gmail.com>
2024-05-14 14:13:41 +0200

    Added An empty Dog class that inherits from Animal class.

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>
+/// An empty class.
+///</summary>
+class Animal
+{
+    
+}
+
+///<summary>
+/// An empty class that inherit from the Animal class.
+///</summary>
+class Dog : Animal
+{
+
+}
