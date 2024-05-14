bed25b54677eaf0875f05b4096316492ecb44fe5
JeremMac <macal.jerem@gmail.com>
2024-05-14 11:22:49 +0200

    Added a method that returns true if an instance is inherited from another class.

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+///<summary>
+/// A class that contains the IsOnlyASubclass method.
+///</summary>
+class Obj
+{
+
+    ///<summary>
+    /// A method that returns True if the object is an instance of a class that inherits from the specified class, otherwise return False.
+    ///</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return true;
+        }
+        return false;
+    }
+}
