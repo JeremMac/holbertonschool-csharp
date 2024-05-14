f909e0c48d02248439ebdcaa0a3ab33872258d78
JeremMac <macal.jerem@gmail.com>
2024-05-14 10:38:04 +0200

    Fix: forgotten documentation.

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
+///<summary>
+/// A class that contains IsOfTypeInt method.
+///</summary>
 class Obj
 {
+    ///<summary>
+    /// A method that return true if obj is an int, otherwise return false.
+    ///</summary>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj is int)
