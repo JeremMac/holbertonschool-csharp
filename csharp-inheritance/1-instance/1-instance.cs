c0710794c1efdfe89cbfb715c21f337717004047
JeremMac <macal.jerem@gmail.com>
2024-05-14 10:57:18 +0200

    Added a method that return true if obj is an Array.

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>
+/// A class that contains the sInstanceOfArray method.
+///</summary>
+class Obj
+{
+
+    ///<summary>
+    /// A method that returns true if obj is an Array or inherits from it. 
+    ///</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+        {
+            return true;
+        }
+        return false;
+    }
+}
