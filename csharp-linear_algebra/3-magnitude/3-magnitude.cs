0caa6c26d6836324323c491f019880b72e7cbe8c
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-05-27 16:06:39 +0200

    Added a program that calculate the lenght of 3d and 2d vectors.

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3_magnitude
+{
+    class VectorMath
+    {
+        public static double Magnitude(double[] vector)
+        {
+            public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
+        {
+            // Vecteur 2D
+            return Math.Round(Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]), 2);
+        }
+        else if (vector.Length == 3)
+        {
+            // Vecteur 3D
+            return Math.Round(Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]), 2);
+        }
+        else
+        {
+            // Vecteur non valide
+            return -1;
+        }
+    }
+        }
+    }
+}
