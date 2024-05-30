63b1cb59a3883fdd5e4484aca6a286306b4293b9
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-05-30 13:27:20 +0200

    Re added parenthesis.

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -17,9 +17,9 @@ class VectorMath
 
         double[] crossPod = new double[3];
 
-        crossPod[0] = vector1[2] * vector2[1] - vector1[1] * vector2[2];
-        crossPod[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
-        crossPod[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+        crossPod[0] = (vector1[2] * vector2[1]) - (vector1[1] * vector2[2]);
+        crossPod[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
+        crossPod[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
 
         return crossPod;
     }
