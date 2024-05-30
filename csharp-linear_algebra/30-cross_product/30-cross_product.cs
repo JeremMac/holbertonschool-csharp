c3dbf584437a6604328f6ecb85c38f95fc331de9
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-05-30 13:50:43 +0200

    Update 30-cross_product.cs

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -17,9 +17,9 @@ class VectorMath
 
         double[] crossPod = new double[3];
 
-        crossPod[0] = Math.Round(vector1[2] * vector2[1] - vector1[1] * vector2[2], 2);
-        crossPod[1] = Math.Round(vector1[2] * vector2[0] - vector1[0] * vector2[2], 2);
-        crossPod[2] = Math.Round(vector1[0] * vector2[1] - vector1[1] * vector2[0], 2);
+        crossPod[0] = Math.Round((vector1[2] * vector2[1]) - (vector1[1] * vector2[2]), 2);
+        crossPod[1] = Math.Round((vector1[2] * vector2[0]) - (vector1[0] * vector2[2]), 2);
+        crossPod[2] = Math.Round((vector1[0] * vector2[1]) - (vector1[1] * vector2[0]), 2);
 
         return crossPod;
     }
