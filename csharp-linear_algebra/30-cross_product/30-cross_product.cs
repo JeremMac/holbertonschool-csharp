89f8845575d86cb4f9e1ff930c2cc07bc2bef0e9
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-05-30 13:30:41 +0200

    Corrected the if condition (=!).

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -10,16 +10,16 @@ class VectorMath
     ///</summary>
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Length =! 3 || vector2.Length =! 3)
+        if (vector1.Length != 3 || vector2.Length =! 3)
         {
             return new double[] {-1};
         }
 
         double[] crossPod = new double[3];
 
-        crossPod[0] = (vector1[2] * vector2[1]) - (vector1[1] * vector2[2]);
-        crossPod[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
-        crossPod[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
+        crossPod[0] = vector1[2] * vector2[1] - vector1[1] * vector2[2];
+        crossPod[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+        crossPod[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
 
         return crossPod;
     }
