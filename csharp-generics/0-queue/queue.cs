011e92eff7625e0f0ea599775125e6c54b934865
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-07-22 16:10:30 +0200

    Added documentation for class and methode.

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,9 +1,17 @@
 ﻿using System;
 
+/// <summary>
+/// A class that contains the CheckType methode.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class Queue<T>
 {
+    /// <summary>
+    /// a methode that returns the Queue’s type.
+    /// </summary>
+    /// <returns> the name of the type </returns>
     public string CheckType()
     {
-        return T;
+        return typeof(T).Name;
     }
 }
