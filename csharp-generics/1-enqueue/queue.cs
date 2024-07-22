57af1d18530b7dbcd8af605df0d4be1930fd2239
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-07-22 16:54:20 +0200

    Added 1-enqueue files for task 1.

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+/// <summary>
+/// A class that contains the CheckType methode.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Queue<T>
+{
+    /// <summary>
+    /// a methode that returns the Queue’s type.
+    /// </summary>
+    /// <returns> the name of the type </returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// a class that sets a new node.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// The value of the node
+        /// </summary>
+        public T value { get; set; }
+
+        /// <summary>
+        /// the node that contains value
+        /// </summary>
+        public Node next { get; set; }
+
+        /// <summary>
+        /// Constructor that sets the value
+        /// </summary>
+        /// <param name="value"></param>
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    // Properties of Queue<T>
+    private Node head;
+    private Node tail;
+    private int count;
+
+    /// <summary>
+    /// Constructor for Queue
+    /// </summary>
+    public Queue()
+    {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+    }
+
+    /// <summary>
+    /// Method to add a new node to the end of the queue.
+    /// </summary>
+    /// <param name="value"></param>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        
+        if (this.tail != null)
+        {
+            this.tail.next = newNode;
+        }
+        this.tail = newNode;
+        
+        if (this.head == null)
+        {
+            this.head = newNode;
+        }
+        
+        this.count++;
+    }
+
+    /// <summary>
+    /// Method to return the number of nodes in the queue
+    /// </summary>
+    /// <returns> Int number of nodes.</returns>
+    public int Count()
+    {
+        return this.count;
+    }
+}
