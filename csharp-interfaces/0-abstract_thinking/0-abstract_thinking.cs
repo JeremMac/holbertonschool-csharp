5011a517b1ec41752dd79a6cc0502683cabcb70c
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-07-23 15:51:53 +0200

    Added Base abstract class for task 1.

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// An abstract class that contains the name property.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// A public string.
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Overiden ToString methode.
+    /// </summary>
+    /// <returns>A caracter string</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
