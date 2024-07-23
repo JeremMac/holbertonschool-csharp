f6ba682244d2ad6098dc5a26b97d5015a7dd4db8
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-07-23 17:11:14 +0200

    Added a Door class accordingly to task 2.

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>
+/// An abstract class that contains the name property.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// A public string.
+    /// </summary>
+    public string name { get; set;}
+
+    /// <summary>
+    /// Overiden ToString methode.
+    /// </summary>
+    /// <returns>A caracter string</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>
+/// A class that represents a door.
+/// </summary>
+public class Door: Base, IInteractive
+{
+    /// <summary>
+    /// Consructor to set the value of door's name.
+    /// </summary>
+    /// <param name="value"></param>
+    public Door(string value = "Door")
+    {
+        name = value;
+    }
+
+    /// <summary>
+    /// A functions that sums an interaction with Door.
+    /// </summary>
+    public void Interact()
+    {
+        Console.WriteLine($@"You try to open the {name}. It's locked.");
+    }
+}
