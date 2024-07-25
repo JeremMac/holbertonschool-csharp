586cf15ddef1ac8d6c96202e211cecc03845dd9b
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-07-25 19:50:18 +0200

    Update 5-eventful.cs

--- csharp-delegates_events/5-eventful/5-eventful.cs
+++ csharp-delegates_events/5-eventful/5-eventful.cs
@@ -23,8 +23,11 @@ public class Player
     private string status;
 
     /// <summary>
-    /// A constructor for Player class that sets HP and name values.
+    /// A constructor for PLayer class that sets
+    /// HP, and name values.
     /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
     public Player(string name = "Player", float maxHp = 100f)
     {
         this.name = name;
@@ -46,7 +49,7 @@ public class Player
     }
 
     /// <summary>
-    /// A method that prints the health of the Player class.
+    /// A methode that print the health of the Player class.
     /// </summary>
     public void PrintHealth()
     {
@@ -54,55 +57,68 @@ public class Player
     }
 
     /// <summary>
-    /// An event that handles CurrentHPArgs event.
+    /// An event that handle CurrentHPArgss envent.
     /// </summary>
     public event EventHandler<CurrentHPArgs> HPCheck;
 
     /// <summary>
-    /// A delegate method that will call TakeDamage or HealDamage.
+    /// A delegate methode that will call TakeDamage or HealDamage.
     /// </summary>
+    /// <param name="amount"></param>
     public delegate void CalculateHealth(float amount);
 
     /// <summary>
     /// Calculate the damage to the player.
     /// </summary>
+    /// <param name="damage"></param>
     public void TakeDamage(float damage)
     {
+        float HPdam;
         if (damage <= 0)
         {
             Console.WriteLine($"{name} takes 0 damage!");
-            return;
+        }
+        else
+        {
+            HPdam = this.hp - damage;
+            Console.WriteLine($"{name} takes {damage} damage!");
+            ValidateHP(HPdam);
         }
 
-        Console.WriteLine($"{name} takes {damage} damage!");
-        ValidateHP(this.hp - damage);
     }
 
     /// <summary>
     /// Calculate the amount of HP healed.
     /// </summary>
+    /// <param name="heal"></param>
     public void HealDamage(float heal)
     {
+        float HPheal;
         if (heal <= 0)
         {
             Console.WriteLine($"{name} heals 0 HP!");
-            return;
+        }
+        else
+        {
+            HPheal = this.hp + heal;
+            Console.WriteLine($"{name} heals {heal} HP!");
+            ValidateHP(HPheal);
         }
 
-        Console.WriteLine($"{name} heals {heal} HP!");
-        ValidateHP(this.hp + heal);
     }
 
     /// <summary>
-    /// A method that will validate the value of hp after damage or heal.
+    /// A methode that will validate the value
+    /// of hp after damage or heal.
     /// </summary>
+    /// <param name="newHp"></param>
     public void ValidateHP(float newHp)
     {
         if (newHp < 0)
         {
             this.hp = 0;
         }
-        else if (newHp > this.maxHp)
+        else if(newHp > this.maxHp)
         {
             this.hp = this.maxHp;
         }
@@ -112,11 +128,35 @@ public class Player
         }
 
         OnCheckStatus(new CurrentHPArgs(this.hp));
+        //OnHPCheck(new CurrentHPArgs(hp));
     }
 
     /// <summary>
-    /// Call CurrentHPArgs event.
+    /// A methode that sets the application of Modifier.
     /// </summary>
+    /// <param name="baseValue"></param>
+    /// <param name="modifier"></param>
+    /// <returns></returns>
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if (modifier == Modifier.Weak)
+        {
+            return baseValue / 2;
+        }
+        else if (modifier == Modifier.Strong)
+        {
+            return baseValue * 1.5f;
+        }
+        else
+        {
+            return baseValue;
+        }
+    }
+
+    /// <summary>
+    /// Call CurrentHPArgs envent.
+    /// </summary>
+    /// <param name="e"></param>
     protected virtual void OnHPCheck(CurrentHPArgs e)
     {
         HPCheck?.Invoke(this, e);
@@ -154,17 +194,19 @@ public class Player
         {
             Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Health has reached zero!");
+            Console.ResetColor();
         }
         else
         {
             Console.ForegroundColor = ConsoleColor.Red; 
             Console.WriteLine("Health is low!");
+            Console.ResetColor();
         }
-        Console.ResetColor();
     }
 
     /// <summary>
-    /// Check player status based on hp value.
+    /// A methode that checks the status of the Player
+    /// based on the Hp value.
     /// </summary>
     /// <param name="e"></param>
     public void OnCheckStatus(CurrentHPArgs e)
@@ -179,27 +221,49 @@ public class Player
 }
 
 /// <summary>
-/// A modifier that will set the statuses weak, strong, or basic.
+/// A modifier that will set the statuses
+/// week , strong or basic.
 /// </summary>
 public enum Modifier
 {
+    /// <summary>
+    /// A state that will divise baseValue by 2.
+    /// </summary>
     Weak,
+
+    /// <summary>
+    /// The default state of baseValue.
+    /// </summary>
     Base,
+
+    /// <summary>
+    /// A state that multiply baseValue by 1.5.
+    /// </summary>
     Strong
 }
 
 /// <summary>
 /// The delegate that will interact with Modifier and ApplyModifier.
 /// </summary>
+/// <param name="baseValue"></param>
+/// <param name="modifier"></param>
+/// <returns></returns>
 public delegate float CalculateModifier(float baseValue, Modifier modifier);
 
 /// <summary>
-/// A class that sets new HP based on events.
+/// A class thet sets newHP based on events.
 /// </summary>
-public class CurrentHPArgs : EventArgs
+public  class CurrentHPArgs : EventArgs
 {
+    /// <summary>
+    /// A float the holds the current Hp value.
+    /// </summary>
     public float currentHp { get; }
 
+    /// <summary>
+    /// Consructor to set all value for CurrentHPArgs class.
+    /// </summary>
+    /// <param name="newHp"></param>
     public CurrentHPArgs(float newHp)
     {
         currentHp = newHp;
