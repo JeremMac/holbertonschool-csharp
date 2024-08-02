3fff9a76c2a639f314415ad3dc84df0039686f79
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-08-02 18:12:23 +0200

    Added InventoryLibrary files.

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        public string Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        public BaseClass()
+        {
+            Id = Guid.NewGuid().ToString();
+            DateCreated = DateTime.Now;
+            DateUpdated = DateTime.Now;
+        }
+    }
+}
+
